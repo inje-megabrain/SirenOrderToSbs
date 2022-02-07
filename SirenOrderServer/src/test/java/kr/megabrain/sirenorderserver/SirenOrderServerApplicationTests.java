53eb9e297dd418ce229c458012d0b0428dfcb430
Hoon9901 <shonn.dev@gmail.com>
2022-02-07 23:32:31 +0900

    Add Server Project

---
+++ SirenOrderServer/src/test/java/kr/megabrain/sirenorderserver/SirenOrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.megabrain.sirenorderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenOrderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
