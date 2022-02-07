0dc7a3a4ac11be37214fa038ce537cfa8f8f0667
Younggun Park <jadru@Youngguns-MacBook-Pro.local>
2022-02-08 01:18:28 +0900

    make submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    Anchor,
+    Button,
+    Footer,
+    Grommet,
+    Header,
+    Heading,
+    Main,
+    Menu,
+    Paragraph,
+    Text,
+    Form,
+    Box,
+    RadioButtonGroup, Select, TextArea
+} from 'grommet';
+import { Home } from 'grommet-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [size, setSize] = React.useState('tall');
+    const [ice, setIce] = React.useState('ICE');
+    const [num, setNumber] = React.useState('1');
+    const [name, setName] = React.useState('shonn');
+    const [menu, setMenu] = React.useState('Cafe Latte');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Ordered: ${name}, ${num} ${size} size ${ice} ${menu}`);
+    };
+    return (
+        <Grommet theme={theme}>
+            <Header background="neutral-3">
+                <Button hoverIndicator icon={<Home />}/>
+                <Menu label="account" items={[{ label: 'user' }, { label: 'admin'}]} />
+            </Header>
+            <Main pad="large">
+                <Heading>Order Coffee ☕️</Heading>
+                <Paragraph>SbsBucks</Paragraph>
+                <Form
+                    onChange={({ value }) => {console.log("Submit: ", value)}}
+                    onSubmit={handleSubmit}
+                >
+                        <Box
+                            direction="row"
+                            border={{ color: 'brand', size: 'small' }}
+                            pad="medium"
+                        >
+                            <Box pad="medium">
+                                <Paragraph>Menu</Paragraph>
+                                <RadioButtonGroup
+                                    name="doc"
+                                    options={['Ice Americano', 'Cafe Latte', 'Cafe Mocha', 'Iced Tea']}
+                                    value={menu}
+                                    onChange={(event) => setMenu(event.target.value)}
+                                />
+                            </Box>
+                            <Box pad="medium">
+                                <Paragraph>ICE or HOT</Paragraph>
+                            <Select
+                                options={['ICE', 'HOT']}
+                                value={ice}
+                                onChange={({ option }) => setIce(option)}
+                            />
+                            </Box>
+                            <Box pad="medium">
+                                <Paragraph>Size</Paragraph>
+                                <Select
+                                    options={['tall', 'grande', 'venti']}
+                                    value={size}
+                                    onChange={({ option }) => setSize(option)}
+                                />
+                            </Box>
+                            <Box pad="medium">
+                                <Paragraph>Number</Paragraph>
+                                <TextArea
+                                    placeholder="type here"
+                                    value={num}
+                                    onChange={event => setNumber(event.target.value)}
+                                />
+                            </Box>
+                        </Box>
+                    <br />
+                    <Box direction="row" gap="medium">
+                        <Button type="submit" primary label="Submit" />
+                    </Box>
+                </Form>
+            </Main>
+            <Footer background="neutral-3" pad="medium">
+                <Text>Copyright</Text>
+                <Anchor label="About" />
+            </Footer>
+        </Grommet>
+    );
 }
 
 export default App;
+
+const theme = {
+    global: {
+        margin: '0px',
+        font: {
+            family: 'Roboto',
+            size: '18px',
+            height: '20px',
+        },
+    },
+};
